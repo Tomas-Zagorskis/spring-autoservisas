8280c6708d299295941a651946040dd3f866a4da
Tomas-Zagorskis <tomzagos@gmail.com>
2022-07-26 12:46:50 +0300

    feat: added path variable for history page

--- src/main/java/lt/codeacademy/springautoservisas/controllers/HistoryController.java
+++ src/main/java/lt/codeacademy/springautoservisas/controllers/HistoryController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
@@ -19,12 +20,13 @@ public class HistoryController {
 
     private HistoryService historyService;
 
-    @GetMapping
+    @GetMapping("/{id}")
     public String showHistoryPage(
             @SortDefault(sort = "regTime", direction = Sort.Direction.DESC)
-            Pageable pageable, Model model, UUID id) {
+            Pageable pageable, Model model,
+            @PathVariable UUID id) {
 
-        model.addAttribute("autos", historyService.getHistoryByClientId(pageable, id));
+        model.addAttribute("history", historyService.getHistoryByClientId(pageable, id));
         return "history";
     }
 }
