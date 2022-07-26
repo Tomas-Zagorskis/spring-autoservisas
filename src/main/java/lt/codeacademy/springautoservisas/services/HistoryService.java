21508c859b8aa8a622a3ec51f9f1e59346e60b9c
Tomas-Zagorskis <tomzagos@gmail.com>
2022-07-26 12:42:28 +0300

    feat: changed List to Page obj

--- src/main/java/lt/codeacademy/springautoservisas/services/HistoryService.java
+++ src/main/java/lt/codeacademy/springautoservisas/services/HistoryService.java
@@ -3,10 +3,10 @@ package lt.codeacademy.springautoservisas.services;
 import lombok.AllArgsConstructor;
 import lt.codeacademy.springautoservisas.entities.HistoryOfAutos;
 import lt.codeacademy.springautoservisas.repos.HistoryRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,7 +15,7 @@ public class HistoryService {
 
     HistoryRepository historyRepository;
 
-    public List<HistoryOfAutos> getHistoryByClientId(Pageable pageable, UUID id) {
+    public Page<HistoryOfAutos> getHistoryByClientId(Pageable pageable, UUID id) {
 
        return historyRepository.findAllByClientId(pageable, id);
     }
