e93d2cc12f1e4cb3be37c9f4268cb28b91817e40
Tomas-Zagorskis <tomzagos@gmail.com>
2022-07-31 20:27:48 +0300

    feat: adding UUID into user table

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,9 +1,9 @@
-INSERT INTO USERS (username, password, email, first_name, last_name, age) VALUES
-    ('admin','admin','admin@gmail.com','admin','admin',66),
-    ('register1','reg123','reg@gmail.com','Kristina','Pavardene',45),
-    ('Petras1985','petriukas','mech@gmail.com','Petras','Petraitis',37),
-    ('Jonasmech','mechanikas','mech2@gmail.com','Jonas','Jonaitis',26),
-    ('register2','123reg123','reg2@gmail.com','Tadas','Zukauskas',30);
+INSERT INTO USERS (id, username, password, email, first_name, last_name, age) VALUES
+    ('111d759c-133d-4ef3-b621-c271804da7b1','admin','admin','admin@gmail.com','admin','admin',66),
+    ('222d759c-133d-4ef3-b621-c271804da7b1','register1','reg123','reg@gmail.com','Kristina','Pavardene',45),
+    ('333d759c-133d-4ef3-b621-c271804da7b1','Petras1985','petriukas','mech@gmail.com','Petras','Petraitis',37),
+    ('444d759c-133d-4ef3-b621-c271804da7b1','Jonasmech','mechanikas','mech2@gmail.com','Jonas','Jonaitis',26),
+    ('555d759c-133d-4ef3-b621-c271804da7b1','register2','123reg123','reg2@gmail.com','Tadas','Zukauskas',30);
 
 INSERT INTO CLIENTS (id, name, surname, email, phone_nr, city, address) VALUES
     ('474d759c-133d-4ef3-b621-c271804da7b1','Petras','Petraitis','admin@gmail.com','8765842135','Kaunas','Rytu 5'),
@@ -32,17 +32,17 @@ INSERT INTO CLIENTS (id, name, surname, email, phone_nr, city, address) VALUES
     ('474d759c-133d-4ef3-b621-c271804da8b3','Tadas','Zukauskas','mech12f@gmail.com','8765842139','Jonava','Kauno 2');
 
 INSERT INTO Autos (plate_nr, registration_time, brand, model, year, issue, fixed, costs, client_id) VALUES
-    ('afs456', '2022-07-21 22:11', 'audi','a6',2005,'sugedes variklis',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
-    ('gfd123','2022-08-02 10:11', 'vw','golf',2012,'skiles stiklas',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
-    ('gfd555','2022-04-15 12:11', 'toyota','raw4',2016,'vaziuokle',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b2'),
-    ('uyt666','2022-07-02 21:11', 'opel','corsa',2002,'pakeist alyva',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b3'),
-    ('qwe845','2022-07-08 08:11', 'audi','q7',2015,'sumontuot ratus',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b4'),
-    ('tre333','2022-07-25 10:11', 'bmw','x5',2009,'pakeist filtrus',false,0.0,'474d759c-133d-4ef3-b621-c271804da8b3');
+    ('afs456', '2022-07-21 22:11', 'audi','a6','2005','sugedes variklis',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
+    ('gfd123','2022-08-02 10:11', 'vw','golf','2012','skiles stiklas',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
+    ('gfd555','2022-04-15 12:11', 'toyota','raw4','2016','vaziuokle',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b2'),
+    ('uyt666','2022-07-02 21:11', 'opel','corsa','2002','pakeist alyva',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b3'),
+    ('qwe845','2022-07-08 08:11', 'audi','q7','2015','sumontuot ratus',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b4'),
+    ('tre333','2022-07-25 10:11', 'bmw','x5','2009','pakeist filtrus',false,0.0,'474d759c-133d-4ef3-b621-c271804da8b3');
 
 INSERT INTO History (id, plate_nr, registration_time, brand, model, year, issue, fixed, costs, client_id) VALUES
-    (91, 'afs456', '2022-07-21 22:11', 'audi','a6',2005,'sugedes variklis',true,69.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
-    (92, 'gfd123','2022-08-02 10:11', 'vw','golf',2012,'skiles stiklas',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
-    (93, 'gfd555','2022-04-15 12:11', 'toyota','raw4',2016,'vaziuokle',true,20.0,'474d759c-133d-4ef3-b621-c271804da7b2'),
-    (94, 'uyt666','2022-07-02 21:11', 'opel','corsa',2002,'pakeist alyva',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b3'),
-    (95, 'qwe845','2022-07-08 08:11', 'audi','q7',2015,'sumontuot ratus',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b4'),
-    (96, 'tre333','2022-07-25 10:11', 'bmw','x5',2009,'pakeist filtrus',false,0.0,'474d759c-133d-4ef3-b621-c271804da8b3');
+    (91, 'afs456', '2022-07-21 22:11', 'audi','a6','2005','sugedes variklis',true,69.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
+    (92, 'gfd123','2022-08-02 10:11', 'vw','golf','2012','skiles stiklas',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b1'),
+    (93, 'gfd555','2022-04-15 12:11', 'toyota','raw4','2016','vaziuokle',true,20.0,'474d759c-133d-4ef3-b621-c271804da7b2'),
+    (94, 'uyt666','2022-07-02 21:11', 'opel','corsa','2002','pakeist alyva',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b3'),
+    (95, 'qwe845','2022-07-08 08:11', 'audi','q7','2015','sumontuot ratus',false,0.0,'474d759c-133d-4ef3-b621-c271804da7b4'),
+    (96, 'tre333','2022-07-25 10:11', 'bmw','x5','2009','pakeist filtrus',false,0.0,'474d759c-133d-4ef3-b621-c271804da8b3');
