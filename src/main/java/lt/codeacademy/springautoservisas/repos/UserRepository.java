45d27ae1451d20d9426c1601fd1db24ddc1eacca
Tomas-Zagorskis <tomzagos@gmail.com>
2022-08-02 21:01:53 +0300

    feat: id type change for users

--- src/main/java/lt/codeacademy/springautoservisas/repos/UserRepository.java
+++ src/main/java/lt/codeacademy/springautoservisas/repos/UserRepository.java
@@ -2,10 +2,16 @@ package lt.codeacademy.springautoservisas.repos;
 
 import lt.codeacademy.springautoservisas.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, String> {
 
-    Long countById(UUID id);
+    Long countByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<User> findUserWithRoles(@Param("username") String username);
 }
--- src/main/java/lt/codeacademy/springautoservisas/services/UserService.java
+++ src/main/java/lt/codeacademy/springautoservisas/services/UserService.java
@@ -2,43 +2,51 @@ package lt.codeacademy.springautoservisas.services;
 
 import lombok.AllArgsConstructor;
 import lt.codeacademy.springautoservisas.entities.User;
-import lt.codeacademy.springautoservisas.repos.UserRepository;
 import lt.codeacademy.springautoservisas.exceptions.UserNotFoundException;
+import lt.codeacademy.springautoservisas.repos.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-@AllArgsConstructor
 @Service
-public class UserService {
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserWithRoles(username)
+                .orElseThrow(() -> new UserNotFoundException("msg.user.not.found", username));
+    }
 
     public List<User> listAll() {
 
-        return (List<User>) userRepository.findAll();
+        return userRepository.findAll();
     }
 
     public void saveUser(User user) {
 
-        user.setId(UUID.randomUUID());
         userRepository.save(user);
     }
 
-    public User getUser(UUID id) throws UserNotFoundException {
+    public User getUser(String id) throws UserNotFoundException {
 
         Optional<User> result = userRepository.findById(id);
         if (result.isPresent()) {
             return result.get();
         }
-        throw new UserNotFoundException("Could not find any user with ID ", id);
+        throw new UserNotFoundException("msg.user.not.found", id);
     }
 
-    public void delete(UUID id) throws UserNotFoundException {
-        Long count = userRepository.countById(id);
+    public void delete(String id) throws UserNotFoundException {
+        Long count = userRepository.countByUsername(id);
         if (count == null || count == 0) {
-            throw new UserNotFoundException("Could not find any user with ID ", id);
+            throw new UserNotFoundException("msg.user.not.found", id);
         }
         userRepository.deleteById(id);
 
