93e54bbb454e60289846e429219cd29854907e37
Tomas-Zagorskis <tomzagos@gmail.com>
2022-08-02 20:58:26 +0300

    feat: new role entity

---
+++ src/main/java/lt/codeacademy/springautoservisas/entities/Role.java
@@ -0,0 +1,24 @@
+package lt.codeacademy.springautoservisas.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    @Id
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return ROLE_PREFIX + name;
+    }
+}
